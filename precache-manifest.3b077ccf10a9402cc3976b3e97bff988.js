f99af5b2c5faf572ede0aa956d10b6d1b54204cd
Jesus Rojas <jesusarvelais1000@hotmail.com>
2019-10-28 12:35:24 -0700

    Updates

---
+++ precache-manifest.3b077ccf10a9402cc3976b3e97bff988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193e85f12a80f68326cf652bf17ad7df",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fd6d5a49b911da99daad",
+    "url": "/robofriends/static/css/main.07be1880.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "fd6d5a49b911da99daad",
+    "url": "/robofriends/static/js/main.f6df7651.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
